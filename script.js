d8d3c56d5d10f8c57d71b51db78d28f590bdb17f
nigadesanket3 <148434823+nigadesanket3@users.noreply.github.com>
2023-10-23 10:10:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-
-number = 5;
- for(i = number; i >= 1; i--)
-    {
-         
-        // Inner loop print space
-        for(j = i; j < number; j++)
+function one()
+{
+    number=5;
+    for(i = number; i >= 1; i--)
         {
-            document.write("&nbsp;");
-        }
-         
-        // Inner loop print star
-        for(j = 1; j <= (2 * i - 1); j++)
-        {
-            document.write("*");
-        }
-         
-        // Ending line after each row
-        document.write("<br>");
-    } 
-
+             
+            // Inner loop print space
+            for(j = i; j < number; j++)
+            {
+                document.write("&nbsp;");
+            }
+             
+            // Inner loop print star
+            for(j = 1; j <= (2 * i - 1); j++)
+            {
+                document.write("*");
+            }
+             
+            // Ending line after each row
+            document.write("<br>");
+        } 
+    
+    document.getElementById("demo").innerHTML = text;
+    
+      }
